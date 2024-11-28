c3d628bb61e8972c7dd561599db47e581aa212c5
Kevin lima pena <kevinlimapena@gmail.com>
2024-11-28 15:10:57 -0400

    melhorias app

---
+++ main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+let win
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // Caminho correto para o index.html dentro do diretório build/web
+  win.loadFile(path.join(__dirname, 'index.html'))
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
